02bc3c47182e478507e997cb5d4285834171c653
Thomas Raimond <thomas.raimond@rednet.io>
2021-10-04 16:03:47 +0200

    chore(scripts): oh-my-zsh separate

--- install.sh
+++ install.sh
@@ -1,6 +1,10 @@
 #!/bin/bash
 
-sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+git clone git@github.com:herrbischoff/iterm2-gruvbox.git /tmp/gruvbox
+
+mv ./.zshrc ~/.zshrc
+
+git clone https://github.com/spaceship-prompt/spaceship-prompt.git "$ZSH_CUSTOM/themes/spaceship-prompt" --depth=1
 
 brew install spaceship thefuck direnv
 
@@ -8,7 +12,6 @@ brew tap homebrew/cask-fonts
 
 brew install --cask font-fira-code iterm2
 
-git clone https://github.com/spaceship-prompt/spaceship-prompt.git "$ZSH_CUSTOM/themes/spaceship-prompt" --depth=1
 
 ln -s "$ZSH_CUSTOM/themes/spaceship-prompt/spaceship.zsh-theme" "$ZSH_CUSTOM/themes/spaceship.zsh-theme"
 
