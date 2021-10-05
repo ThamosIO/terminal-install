445006bd8c938ea15080ea391e6ff51e85453f7d
Thomas Raimond <thomas.raimond@rednet.io>
2021-10-05 10:01:35 +0200

    refactor(install): better error handling

--- install.sh
+++ install.sh
@@ -1,18 +1,25 @@
 #!/bin/bash
 
-git clone git@github.com:herrbischoff/iterm2-gruvbox.git /tmp/gruvbox
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
 
-mv ./.zshrc ~/.zshrc
+cp -f ./.zshrc ~/.zshrc
 
-git clone https://github.com/spaceship-prompt/spaceship-prompt.git "$ZSH_CUSTOM/themes/spaceship-prompt" --depth=1
+source ~/.zshrc
 
-brew install spaceship thefuck direnv
+brew install --ignore-already-installed spaceship thefuck direnv
 
 brew tap homebrew/cask-fonts
 
-brew install --cask font-fira-code iterm2
+brew install --cask --ignore-already-installed font-fira-code iterm2
 
+PATH=$ZSH_CUSTOM
 
+[ -z "$ZSH_CUSTOM" ] && exit 1
+
+git clone https://github.com/spaceship-prompt/spaceship-prompt.git "$ZSH_CUSTOM/themes/spaceship-prompt" --depth=1
 ln -s "$ZSH_CUSTOM/themes/spaceship-prompt/spaceship.zsh-theme" "$ZSH_CUSTOM/themes/spaceship.zsh-theme"
 
-mv ./.zshrc ~/.zshrc
+cp -f ./.zshrc ~/.zshrc
+
+mkdir -p $ZSH_CUSTOM/colors/
+git clone git@github.com:herrbischoff/iterm2-gruvbox.git $ZSH_CUSTOM/colors/gruvbox
